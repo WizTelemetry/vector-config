5d050002b30ae187547cb2b5364c04302e4ea6b2
Gentleelephant <1132960613@qq.com>
2026-02-12 15:53:48 +0800

    fix(ctrl): disable metrics and health probes in manager setup

--- cmd/main.go
+++ cmd/main.go
@@ -80,10 +80,12 @@ func main() {
 	setupLog.Info("current fileDir is " + constants.FileDir)
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:           scheme,
-		Port:             9443,
-		LeaderElection:   enableLeaderElection,
-		LeaderElectionID: "66c47c3e.kubesphere.io",
+		MetricsBindAddress:     "0",
+		HealthProbeBindAddress: "0",
+		Scheme:                 scheme,
+		Port:                   9443,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "66c47c3e.kubesphere.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
